3d9c7f4b926001889ab855419711713c4fd889ff
4-abhi <abhi08127@gmail.com>
2020-09-15 22:51:27 +0530

    changes into app config packag.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require('cors');
-
+const path = require("path");
 
 
 const authRoutes = require("./routes/authRoutes");
@@ -23,6 +23,13 @@ app.use("/api/metting", mettingRoutes);
 app.use("/api/mettingUser", mettingUser);
 
 const port = process.env.PORT || 4004;
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+
+}
 app.listen(port, () => {
     console.log("Process start in 4004");
 })
